64042657a13eec13a4b025e9000a7386dfc6ca0b
Mauricio Santelices <m.santelicesa@gmail.com>
2023-03-21 20:30:51 -0300

    actualziados test de ejemplo

--- src/funciones.js
+++ src/funciones.js
@@ -1,9 +1,17 @@
-const sumar = (a, b)=> {
-    return a + b
+const sumar = (num1, num2)=> {
+    if(typeof num1 !== 'number' || typeof num2 !== 'number') {
+        return 'Los argumentos deben ser números'
+    }
+
+    return num1 + num2
 }
 
-const filtrarArreglo = (arr, item)=> {
-    return arr.filter((element)=> element !== item)
+const filtrarArreglo = (arr, element)=> arr.filter((elem)=> elem != element)
+
+const crearSaludo = (msg)=> {
+    if(!msg) throw new Error('Argumento requerido')
+
+    return `Hola ${msg}`
 }
 
-export { sumar, filtrarArreglo }
+export { sumar, filtrarArreglo, crearSaludo }
--- tests/funciones.spec.js
+++ tests/funciones.spec.js
@@ -1,19 +1,63 @@
-import { sumar, filtrarArreglo } from '../src/funciones.js'
+import { sumar, filtrarArreglo, crearSaludo } from '../src/funciones.js'
 
-describe('Pruebas para el archivo funciones.js', ()=> {
-    test('La función sumar debe retornar la suma de sus argumentos', ()=> {
+describe("Pruebas para la funcion sumar", ()=> {
+    // toBe -> Boolean, Number, Strings
+    it('La función sumar debe retornar 3 si sus argumentos son 1 y 2', ()=> {
         const resultado = sumar(1, 2)
+
         expect(resultado).toBe(3)
+        
     })
 
-    test('La función filtrarArreglo debe retornar un arreglo sin el elemento especificado', ()=> {
-        const arr = [1, 2, 3, 4, 5]
-        const item = 4
-        const filtrado = filtrarArreglo(arr, item)
+    it('La función sumar debe retornar un error si sus argumentos no son numeros', ()=> {
+        const resultado = sumar('1')
 
-        expect(filtrado).not.toContain(item)
+        expect(resultado).toBe('Los argumentos deben ser números')
     })
 })
 
+describe("Pruebas para la función filtrarArreglo", ()=> {
+    // toEqual -> Arrays y Objetos
+    it("Debe retornar un nuevo array sin el elemento especificado", ()=> {
+        const arr = [1, 2, 3, 4, 5]
+        const elemento = 4
+        const filtrado = filtrarArreglo(arr, elemento)
 
+        expect(filtrado).toEqual([1, 2, 3, 5])
+    })
 
+    // toContain -> Si un elemento contiene a otro
+    // not -> Sirve para negar a expresión
+    it("Debe retornar un nuevo array que no contenga el elemento especificado", ()=> {
+        const arr = [1, 2, 3, 4, 5]
+        const elemento = 4
+        const filtrado = filtrarArreglo(arr, elemento)
+
+        expect(filtrado).not.toContain(elemento)
+    })
+
+    // toHaveLength -> Length de un Array
+    it("Si el array original tiene 5 elementos, debe retornar un nuevo array con 4 elementos", ()=> {
+        const arr = [1, 2, 3, 4, 5]
+        const elemento = 4
+        const filtrado = filtrarArreglo(arr, elemento)
+
+        expect(filtrado).toHaveLength(4)
+    })
+})
+
+describe("Pruebas para la función crearSaludo", ()=> {
+    // toMatch -> Match de una String con una expresión regular
+    it("Toma una palabra como argumento y retorna un mensaje que la contiene", ()=> {
+        const saludo = crearSaludo('mundo')
+
+        expect(saludo).toMatch(/mundo/)
+        expect(saludo).not.toMatch(/[0-9]/)
+    })
+
+    // toThrow -> Arroja un error
+    it("Retorna un error si se ejecuta sin argumentos", ()=> {
+        // Escuchamos la ejecución de la función
+        expect( ()=> crearSaludo() ).toThrow()
+    })
+})
